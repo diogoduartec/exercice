7019ea052e1e06d42192366fc0ff9c367be73034
diogoduartemb <diogo.costa@mercadobitcoin.com.br>
2022-04-08 12:35:45 -0400

    [FEATURE] add connected convert view

--- xml_converter/views.py
+++ xml_converter/views.py
@@ -1,10 +1,14 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 
+from xml_converter.forms import UploadXMLForm
+from xml_converter.tools import convert_from_xml_to_dict
+
 
 def upload_page(request):
     if request.method == 'POST':
-        # TODO: Convert the submitted XML file into a JSON object and return to the user.
-        return JsonResponse({})
-
+        form = UploadXMLForm(request.POST, request.FILES)
+        if form.is_valid():
+            file_converted = convert_from_xml_to_dict(request.FILES['file'])
+            return JsonResponse(file_converted)
     return render(request, "upload_page.html")
